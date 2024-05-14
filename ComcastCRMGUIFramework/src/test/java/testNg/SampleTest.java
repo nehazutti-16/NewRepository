b51f285c1aadcdb6a86ed4d9314346ac750b5d60
NEHA ZUTTI <NEHA ZUTTI@LAPTOP-UHSOF6P6>
2024-05-14 14:52:29 +0530

    commit

--- ComcastCRMGUIFramework/src/test/java/testNg/SampleTest.java
+++ ComcastCRMGUIFramework/src/test/java/testNg/SampleTest.java
@@ -3,16 +3,14 @@ package testNg;
 import org.testng.annotations.Test;
 
 public class SampleTest {
-	//welcome
-	@Test(dependsOnMethods = "test2", priority=0)
-	public void test1()
-	{
+	// welcome
+	@Test(dependsOnMethods = "test2", priority = 0)
+	public void test1() {
 		System.out.println("t1");
 	}
-	
-	@Test(priority= -1)
-	public void test2()
-	{
+
+	@Test(priority = -1)
+	public void test2() {
 		System.out.println("t2");
 	}
 }
